2de74020a74248536cd04eb09eb4081e716dab94
tomasnunes01 <tomasmnunes@gmail.com>
2022-04-22 23:30:31 +0100

    Revert "added picker and checkbox to AddUser"
    This reverts commit d0555273197d24bef066a2fe9e959e322caeaed4.

--- src/screens/AddUser.js
+++ src/screens/AddUser.js
@@ -40,7 +40,7 @@ export const AddUser = ({navigation}) => {
     const [isRendering, setRendering] = useState(false)
 
     const [dataSource, setDataSource] = useState([]);
-    const [pickerValueHolder, setPickerValueHolder] = useState(null);
+    const [pickerValueHolder, setPickerValueHolder] = useState(' ');
 
     const emailInput = React.createRef();
     const nomeInput = React.createRef();
@@ -110,7 +110,6 @@ export const AddUser = ({navigation}) => {
 
     const guardar = () => {
         if (validar()) {
-            
             setLoading(true)
             const cod_escritorio = escritorioService.findOne(pickerValueHolder)
 
@@ -121,9 +120,9 @@ export const AddUser = ({navigation}) => {
                 grupo: grupo,
                 username: username,
                 pass: password,
-                cod_escritorio: pickerValueHolder,
+                cod_escritorio: cod_escritorio,
             }
-            console.log(data)
+            
             userService.registar(data)
                 .then((response) => {
                     setLoading(false)
@@ -232,16 +231,13 @@ export const AddUser = ({navigation}) => {
                                         ref={passwordInput}
                                         maxLength={255}
                                     />
-                                    <View style={{flexDirection: 'row', marginTop: "-5%"}}>
+                                    <View style={{flexDirection: 'row'}}>
                                         <Picker
                                             selectedValue={pickerValueHolder}
                                             onValueChange={(itemValue, itemIndex) => setPickerValueHolder(itemValue)}
                                             mode={"dropdown"}
                                             style={styles.picker}
                                         >
-                                            {!pickerValueHolder &&
-                                                <Picker.Item label='Selecione'/>
-                                            }
                                             { dataSource.map((item, key)=>(
                                                 <Picker.Item label={item.morada} value={item.cod_escritorio} key={key} />)
                                             )}
